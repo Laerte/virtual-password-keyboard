0442407e1d6a5d2a5ca7d4c0b7b7aab3cb9f3122
Laerte <laertefbk@gmail.com>
2020-03-17 22:17:42 -0300

    Generating elements

--- virtualPasswordKeyboard.js
+++ virtualPasswordKeyboard.js
@@ -1,7 +1,42 @@
 class VirtualPasswordKeyboard extends HTMLElement {
     constructor(){
         super();
+        this.className = 'keyboard';
+        this.currentPassword = [];
+        this.digitGroups = generateDigitsGroups();
+
+        for (var i = 0; i < 5; i++) {
+            let digits = this.digitGroups[i];
+            this.innerHTML += `<div class='virtual-keyboard-key' data-group-key=${i}>${digits.join(" or ")}</div>`;
+        }
+
+        this.innerHTML += `<div class='virtual-keyboard-key'>Clear</div>`;
     }
+
+    generateDigitsGroups() {
+        let digitGroups = {};
+
+        for (var i = 0; i < 5; i++) {
+          digitGroups[i] = [];
+          for (var y = 0; y < 2; y++) {
+            // get all numbers generated until now
+            let onlyNumbers = Object.keys(digitGroups)
+                              .map((groupKey) => digitGroups[groupKey])
+                              .flat();
+
+            let randomNumber = Math.floor(Math.random() * 10);
+
+            // verify if number is already taken
+            while (onlyNumbers.indexOf(randomNumber) != -1) {
+              randomNumber = Math.floor(Math.random() * 10);
+            }
+
+            digitGroups[i].push(randomNumber);
+          }
+        }
+
+        return digitGroups;
+      }
 }
 
 window.customElements.define('virtual-password-keyboard', VirtualPasswordKeyboard);
